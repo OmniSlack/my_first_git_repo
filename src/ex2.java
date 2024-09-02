c955573f564579a3bc02a01ae903d9b6c6f3b816
mromnivorous <mromnivorous@gmail.com>
2024-09-02 10:32:37 +0300

    exercises from lecture 2

---
+++ src/ex2.java
@@ -0,0 +1,10 @@
+public class ex2 {
+
+        public static void main(String[] args) {
+            int a = 7;
+            int b = 8;
+            int c = 9;
+
+            System.out.println(a +b + c);
+        }
+}
