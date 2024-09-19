9f48912862487b7d6fc198739c1a6d2f4bf08d3f
mromnivorous <mromnivorous@gmail.com>
2024-09-19 14:24:14 +0300

    exercises from lecture 5,9 & 10

--- src/Main1.java
+++ src/Main1.java
@@ -1,7 +1,7 @@
 
 
-public class Main {
-    public static void main(String... args) {
+public class Main1 {
+    public static void main1(String... args) {
 
         String name = "Martin";
         String middleName = "Jordanov";
